8fba27d40b544ea6117ea1934973d8aa74754a51
azardilis <azardilis.gmail.com>
2011-11-26 17:02:03 +0000

    added main dispatch, functionality to traverse-and and traverse-or, works fine

--- evaluate-formula.scm
+++ evaluate-formula.scm
@@ -3,10 +3,11 @@
     (not (pair? x))))
 
 (define evaluate-formula 
-  (lambda (expr)
-  ;do the regular dispatching here
-  expr
-))
+  (lambda (expr alist)
+    (cond ((atom? expr) (find-binding expr alist))
+          ((not-expr? expr) (not (evaluate-formula (cadr expr) alist)))
+          ((or-expr? expr) (traverse-or-args (cdr expr) alist))
+          ((and-expr? expr) (traverse-and-args (cdr expr) alist)))))
 
 
 ;given a symbol and an a-list, should return its binding in the list
@@ -17,10 +18,6 @@
         (if (eq? (caar alist) symbol)
             (cdr (car alist))
             (find-binding symbol (cdr alist))))))
-
-
-
-
 
 (define not-expr?
   (lambda (expr)
@@ -34,16 +31,26 @@
          (eq? (car expr) 'or)
          (> (length (cdr expr)) 1))))
 
-
 (define and-expr? 
   (lambda (expr)
     (and (not (atom? expr))
          (eq? (car expr) 'and)
          (> (length (cdr expr)) 1))))
 
+;traverses the arguments and applies logical and to them
 (define traverse-and-args
-  (lambda (arg-list)
-    arg-list
-    ;logical and of all the arguments 
-    ;of an and expressions
-))
+  (lambda (arg-list alist)
+    (if (null? arg-list)
+        #t
+        (if (evaluate-formula (car arg-list) alist)
+            (traverse-and-args(cdr arg-list) alist)
+            #f))))
+
+;traverses the arguments and applies logical or to them
+(define traverse-or-args 
+  (lambda (arg-list alist)
+    (if (null? arg-list)
+        #f
+        (if (evaluate-formula (car arg-list) alist)
+            #t
+            (traverse-or-args(cdr arg-list) alist)))))
