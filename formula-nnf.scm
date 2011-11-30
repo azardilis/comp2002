553c78041ffbf3d67a9e95579c0f939aad5f048b
azardilis <azardilis.gmail.com>
2011-11-30 19:34:52 +0000

    added fourth question, works on initial tests (ignore previous commit:p)

---
+++ formula-nnf.scm
@@ -0,0 +1,84 @@
+;passes first tests have to do some further testing though
+; and make it prettier:p
+
+(define atom? 
+  (lambda (exp)
+    (not (pair? exp))))
+
+(define formula->nnf
+  (lambda (exp)
+    (cond ((atomic-prop? exp) exp)
+          ((unary-prop? exp) (if (atom? (cadr exp))
+                                   exp
+                                   (formula->nnf (normalize exp))))
+          ((binary-prop? exp) (cons (car exp) (traverse-args (cdr exp)))))))
+
+(define unary-prop?
+  (lambda (expr)
+    (and (eq? (car expr) 'not)
+         (= (length (cdr expr)) 1))))
+
+(define binary-prop?
+  (lambda (expr)
+    (and (or (eq? (car expr) 'and)
+             (eq? (car expr) 'or))
+         (> (length (cdr expr)) 1))))
+           
+(define atomic-prop?
+  (lambda (expr)
+    (and (atom? expr)
+         (symbol? expr)
+         (not (or (eq? expr 'and)
+                  (eq? expr 'or)
+                  (eq? expr 'not)))
+         (not (null? expr)))))
+
+(define traverse-args
+  (lambda (arg-list)
+    (cond ((null? arg-list) '())
+          (else (cons (formula->nnf (car arg-list)) (traverse-args (cdr arg-list)))))))
+
+(define normalize
+  (lambda (exp)
+    (cond ((double-negation? exp) (not-operand exp))
+          ((not-and? exp) (make-or (cadr exp)))
+          ((not-or? exp) (make-and (cadr exp)))
+          (else exp))))
+
+(define not-or?
+  (lambda (exp)
+    (cond ((binary-prop? (cadr exp))
+           (eq? (caadr exp) 'or))
+          (else #f))))
+  
+(define not-and?
+  (lambda (exp)
+    (cond ((binary-prop? (cadr exp))
+           (eq? (caadr exp) 'and))
+          (else #f))))
+
+(define double-negation? 
+  (lambda (exp)
+    (eq? (operator exp) (car (cadr exp)))))
+
+(define not-operand
+  (lambda (exp)
+    (cadr (cadr exp))))
+
+(define make-or
+  (lambda (exp)
+    (cons 'or (make-nots (cdr exp)))))
+
+(define make-and
+  (lambda (exp)
+    (cons 'and (make-nots (cdr exp)))))
+                      
+(define operator
+  (lambda (exp)
+    (car exp)))
+
+(define make-nots
+  (lambda (exp)
+    (if (null? exp)
+        '()
+        (cons (list 'not (car exp)) (make-nots (cdr exp))))))
